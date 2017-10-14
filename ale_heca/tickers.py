3e37f1a0f318b0a06183712c16bef34ef0189ce0
s1korrrr <s1korrrr@gmail.com>
2017-10-14 21:16:44 +0200

    major update, finally ML working

--- ale_heca/tickers.py
+++ ale_heca/tickers.py
@@ -1,4 +1,5 @@
 import bs4 as bs
+from collections import Counter
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -8,9 +9,9 @@ import pandas as pd
 import pandas_datareader.data as web
 import pickle
 import requests
-import csv
+from sklearn import svm, model_selection, neighbors
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 from time import sleep
-
 
 style.use('ggplot')
 
@@ -111,7 +112,7 @@ def visualize_data():
 
 def process_data_for_labels(ticker):
     hm_days = 7
-    df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
+    df = pd.read_csv('sp500_joined_closed.csv', index_col=0)
     tickers = df.columns.values.tolist()
     df.fillna(0, inplace=True)
 
@@ -121,11 +122,103 @@ def process_data_for_labels(ticker):
     df.fillna(0, inplace=True)
     return tickers, df
 
-# process_data_for_labels("XOM")
+
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+    for col in cols:
+        if col > requirement:
+            return 1
+        if col < -requirement:
+            return -1
+    return 0
+
+
+# def extract_featuresets(ticker):
+#     tickers, df = process_data_for_labels(ticker)
+#     df['{}_target'.format(ticker)] = list(
+#         map(buy_sell_hold, *[df['{}_{}d'.format(ticker, i)] for i in range(1, hm_days + 1)]))
+#
+#     vals = df['{}_target'.format(ticker)].values.tolist()
+#     str_vals = [str(i) for i in vals]
+#
+#     df.fillna(0, inplace=True)
+#
+#
+#     df = df.replace([np.inf, -np.inf], np.nan)  # handling crazy values FE. 0 to 1
+#     df.dropna(inplace=True)
+#
+#     df_vals = df[[ticker for ticker in tickers]].pct_change()
+#     df_vals = df_vals.replace([np.inf, -np.inf], 0)
+#     df_vals.fillna(0, inplace=True)
+#
+#     X = df_vals.values
+#     y = df['{}_target'.format(ticker)].values
+#
+#     return X, y, df
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map(buy_sell_hold,
+                                              df['{}_1d'.format(ticker)],
+                                              df['{}_2d'.format(ticker)],
+                                              df['{}_3d'.format(ticker)],
+                                              df['{}_4d'.format(ticker)],
+                                              df['{}_5d'.format(ticker)],
+                                              df['{}_6d'.format(ticker)],
+                                              df['{}_7d'.format(ticker)]))
+
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return X, y, df
+
+
+def do_ml(ticker):
+    X, y, df = extract_featuresets(ticker)
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X,
+                                                                        y,
+                                                                        test_size=0.25)
+
+    # clf = neighbors.KNeighborsClassifier()
+
+    clf = VotingClassifier([('lsvc', svm.LinearSVC()),
+                            ('knn', neighbors.KNeighborsClassifier()),
+                            ('rfor', RandomForestClassifier())])
+
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print('accuracy:', confidence)
+    predictions = clf.predict(X_test)
+    print('predicted class counts:', Counter(predictions))
+    print()
+    print()
+    return confidence
+
+extract_featuresets('AEP')
+# # examples of running:
+# do_ml('XOM')
+do_ml(['AAPL'])
+# do_ml('ABT')
+# compile_data()
+# process_data_for_labels("APPL")
 
 
 # save_sp500_tickers()
 # get_data_from_yahoo()
-# compile_data()
 # compile_data()
 # visualize_data()
